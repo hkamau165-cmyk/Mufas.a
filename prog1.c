c056eb0d3806c0e2da0e787c64f557b9c1b0e3e8
hkamau165-cmyk <hkamau165@gmail.com>
2026-09-18 10:44:54 +0300

    Add files via upload

---
+++ prog1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    // Name: Humphrey Kamau 
+    // Reg No: CT100/G/30682/26
+
+    printf("Units offered this semester\n");
+    printf("1. Introduction to programming and algorithms\n");
+    printf("2. Data structure\n");
+    printf("3. Database management system\n");
+    printf("4. Software engineering\n");
+
+    return 0;
+}
